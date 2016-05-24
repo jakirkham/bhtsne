17d2fb802f5814c45cfac64264b0a9ffcfc02283
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-05-24 12:12:14 -0400

    conda.recipe: Add a recipe to build `bhtsne`.

---
+++ conda.recipe/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+# FIXME: This is a hack to make sure the environment is activated.
+# The reason this is required is due to the conda-build issue
+# mentioned below.
+#
+# https://github.com/conda/conda-build/issues/910
+#
+source activate "${CONDA_DEFAULT_ENV}"
+
+# Source and binary
+SOURCE_FILES="`ls *.cpp | tr '\r\n' ' '`"
+BIN_FILE="bh_tsne"
+PY_BIN_FILE="bhtsne"
+
+# Compiler flags
+export CFLAGS="-O2"
+export CXXFLAGS="${CFLAGS}"
+
+# Compile the binary
+c++ $SOURCE_FILES -o $BIN_FILE
+
+# Place the binary in the prefix
+mkdir -p "${PREFIX}/bin"
+mv $BIN_FILE "${PREFIX}/bin"
+mv "${PY_BIN_FILE}.py" "${SP_DIR}"
+ln -s "${PREFIX}/bin/${BIN_FILE}" "${SP_DIR}/${BIN_FILE}"
+
+cat >"${PREFIX}/bin/${PY_BIN_FILE}" <<EOF
+#!/usr/bin/env python
+
+import runpy
+runpy.run_module("${PY_BIN_FILE}", run_name="__main__")
+EOF
+
+chmod +x "${PREFIX}/bin/${PY_BIN_FILE}"
